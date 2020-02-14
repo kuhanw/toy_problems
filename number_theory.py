b54e9360d6b90c1ee1b6e1b6c47319cd4b102eef
Kuhan Wang <kuhanw@gmail.com>
2020-02-14 13:24:00 -0500

    Update number_theory.py

--- number_theory.py
+++ number_theory.py
@@ -1,5 +1,19 @@
 #basic sieve
+from collections import defaultdict
+def create_list(): return []
+cmps = defaultdict(create_list)
 
 def generate_primes():
 
   v=2
+  while True:
+    yield v
+    cmps[v*v].append(v)
+    v+=1
+    
+    while v in cmps:
+      for f in cmps[v]:
+        cmps[v+f].append(f)
+        
+      del cmps[v]
+      v+=1
